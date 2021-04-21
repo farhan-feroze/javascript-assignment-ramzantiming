c11ac420000c4807adb41bb21333529c722c952e
farhan-feroze <farhan.feroze005@gmail.com>
2021-04-21 15:49:16 +0500

    all done

---
+++ apps.js
@@ -0,0 +1,92 @@
+var ramzanDate = +prompt("Please enter ramzan date you wish to know the timing for")
+
+if(ramzanDate===1){
+    document.write("<center></center><center></center><h3>Date : 14 April 2021 </h3> <br><b>Sehri:</b> 04:51 AM<br><br> <b>Iftar:</b> 6:55 PM</center>")
+}
+if(ramzanDate===2){
+    document.write("<center><h3>Date : 15 April 2021 </h3> <br><b>Sehri:</b> 04:50 AM<br><br> <b>Iftar:</b> 6:55 PM</center>")
+}
+if(ramzanDate===3){
+    document.write("<center><h3>Date : 16 April 2021 </h3> <br><b>Sehri:</b> 04:49 AM<br><br> <b>Iftar:</b> 6:56 PM</center>")
+}
+if(ramzanDate===4){
+    document.write("<center><h3>Date : 17 April 2021 </h3> <br><b>Sehri:</b> 04:48 AM<br><br> <b>Iftar:</b> 6:56 PM</center>")
+}
+if(ramzanDate===5){
+    document.write("<center><h3>Date : 18 April 2021 </h3> <br><b>Sehri:</b> 04:47 AM<br><br> <b>Iftar:</b> 6:57 PM</center>")
+}
+if(ramzanDate===6){
+    document.write("<center><h3>Date : 19 April 2021 </h3> <br><b>Sehri:</b> 04:46 AM<br><br> <b>Iftar:</b> 6:57 PM</center>")
+}
+if(ramzanDate===7){
+    document.write("<center><h3>Date : 20 April 2021 </h3> <br><b>Sehri:</b> 04:45 AM<br><br> <b>Iftar:</b> 6:57 PM</center>")
+}
+if(ramzanDate===8){
+    document.write("<center><h3>Date : 21 April 2021 </h3> <br><b>Sehri:</b> 04:44 AM<br><br> <b>Iftar:</b> 6:58 PM</center>")
+}
+if(ramzanDate===9){
+    document.write("<center><h3>Date : 22 April 2021 </h3> <br><b>Sehri:</b> 04:43 AM<br><br> <b>Iftar:</b> 6:58 PM</center>")
+}
+if(ramzanDate===10){
+    document.write("<center><h3>Date : 23 April 2021 </h3> <br><b>Sehri:</b> 04:42 AM<br><br> <b>Iftar:</b> 6:59 PM</center>")
+}
+if(ramzanDate===11){
+    document.write("<center><h3>Date : 24 April 2021 </h3> <br><b>Sehri:</b> 04:41 AM<br><br> <b>Iftar:</b> 6:59 PM</center>")
+}
+if(ramzanDate===12){
+    document.write("<center><h3>Date : 25 April 2021 </h3> <br><b>Sehri:</b> 04:40 AM<br><br> <b>Iftar:</b> 7:00 PM</center>")
+}
+if(ramzanDate===13){
+    document.write("<center><h3>Date : 26 April 2021 </h3> <br><b>Sehri:</b> 04:39 AM<br><br> <b>Iftar:</b> 7:00 PM</center>")
+}
+if(ramzanDate===14){
+    document.write("<center><h3>Date : 27 April 2021 </h3> <br><b>Sehri:</b> 04:38 AM<br><br> <b>Iftar:</b> 7:01 PM</center>")
+}
+if(ramzanDate===15){
+    document.write("<center><h3>Date : 28 April 2021 </h3> <br><b>Sehri:</b> 04:37 AM<br><br> <b>Iftar:</b> 7:01 PM</center>")
+}
+if(ramzanDate===16){
+    document.write("<center><h3>Date : 29 April 2021 </h3> <br><b>Sehri:</b> 04:36 AM<br><br> <b>Iftar:</b> 7:02 PM</center>")
+}
+if(ramzanDate===17){
+    document.write("<center><h3>Date : 30 April 2021 </h3> <br><b>Sehri:</b> 04:35 AM<br><br> <b>Iftar:</b> 7:02 PM</center>")
+}
+if(ramzanDate===18){
+    document.write("<center><h3>Date : 01 May 2021 </h3> <br><b>Sehri:</b> 04:34 AM<br><br> <b>Iftar:</b> 7:03 PM</center>")
+}
+if(ramzanDate===19){
+    document.write("<center><h3>Date : 02 May 2021 </h3> <br><b>Sehri:</b> 04:33 AM<br><br> <b>Iftar:</b> 7:03 PM</center>")
+}
+if(ramzanDate===20){
+    document.write("<center><h3>Date : 03 May 2021 </h3> <br><b>Sehri:</b> 04:32 AM<br><br> <b>Iftar:</b> 7:04 PM</center>")
+}
+if(ramzanDate===21){
+    document.write("<center><h3>Date : 04 May 2021 </h3> <br><b>Sehri:</b> 04:31 AM<br><br> <b>Iftar:</b> 7:04 PM</center>")
+}
+if(ramzanDate===22){
+    document.write("<center><h3>Date : 05 May 2021 </h3> <br><b>Sehri:</b> 04:30 AM<br><br> <b>Iftar:</b> 7:05 PM</center>")
+}
+if(ramzanDate===23){
+    document.write("<center><h3>Date : 06 May 2021 </h3> <br><b>Sehri:</b> 04:29 AM<br><br> <b>Iftar:</b> 7:05 PM</center>")
+}
+if(ramzanDate===24){
+    document.write("<center><h3>Date : 07 May 2021 </h3> <br><b>Sehri:</b> 04:28 AM<br><br> <b>Iftar:</b> 7:06 PM</center>")
+}
+if(ramzanDate===25){
+    document.write("<center><h3>Date : 08 May 2021 </h3> <br><b>Sehri:</b> 04:28 AM<br><br> <b>Iftar:</b> 7:06 PM</center>")
+}
+if(ramzanDate===26){
+    document.write("<center><h3>Date : 09 May 2021 </h3> <br><b>Sehri:</b> 04:27 AM<br><br> <b>Iftar:</b> 7:07 PM</center>")
+}
+if(ramzanDate===27){
+    document.write("<center><h3>Date : 10 May 2021 </h3> <br><b>Sehri:</b> 04:26 AM<br><br> <b>Iftar:</b> 7:07 PM</center>")
+}
+if(ramzanDate===28){
+    document.write("<center><h3>Date : 11 May 2021 </h3> <br><b>Sehri:</b> 04:25 AM<br><br> <b>Iftar:</b> 7:08 PM</center>")
+}
+if(ramzanDate===29){
+    document.write("<center><h3>Date : 12 May 2021 </h3> <br><b>Sehri:</b> 04:24 AM<br><br> <b>Iftar:</b> 7:08 PM</center>")
+}
+if(ramzanDate===30){
+    document.write("<center><h3>Date : 13 May 2021 </h3> <br><b>Sehri:</b> 04:24 AM<br><br> <b>Iftar:</b> 7:09 PM</center>")
+}
